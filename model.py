3bbe4aeee93893240ee15f5fa32a43c04cd3453c
leftthomas <leftthomas@qq.com>
2020-02-18 12:23:21 +0800

    refactor model

--- model.py
+++ model.py
@@ -6,20 +6,22 @@ from backbone import eca_mobilenet_v2
 
 
 class Model(nn.Module):
-    def __init__(self, ensemble_size, meta_class_size, feature_dim, share_type='none'):
+    def __init__(self, ensemble_size, meta_class_size, feature_dim, share_type='block5'):
         super(Model, self).__init__()
 
         # configs
         self.ensemble_size = ensemble_size
         module_names = ['conv', 'block1', 'block2', 'block3', 'block4', 'block5', 'block6', 'block7', 'last_conv']
+        if share_type != 'none':
+            common_module_names = module_names[:module_names.index(share_type) + 1]
+            individual_module_names = module_names[module_names.index(share_type) + 1:]
+        else:
+            common_module_names, individual_module_names = [], module_names
 
         # common features
-        if share_type != 'none':
-            common_module_names = module_names[:module_names.index(share_type)]
-        else:
-            common_module_names = []
         self.head = []
-        for name, module in eca_mobilenet_v2(pretrained=True).named_modules():
+        for name, module in eca_mobilenet_v2(pretrained=True, last_channel=feature_dim,
+                                             num_classes=meta_class_size).named_modules():
             if name in common_module_names:
                 self.head.append(module)
         self.head = nn.Sequential(*self.head)
@@ -29,21 +31,31 @@ class Model(nn.Module):
         # individual features
         self.tails = []
         for i in range(ensemble_size):
-            self.tails.append(mobilenet_v2(pretrained=True).features)
+            tail = []
+            for name, module in eca_mobilenet_v2(pretrained=True, last_channel=feature_dim,
+                                                 num_classes=meta_class_size).named_modules():
+                if name in individual_module_names:
+                    tail.append(module)
+            tail = nn.Sequential(*tail)
+            self.tails.append(tail)
         self.tails = nn.ModuleList(self.tails)
         print("# trainable individual feature parameters:",
               sum(param.numel() if param.requires_grad else 0 for param in
                   self.tails.parameters()) // ensemble_size)
 
-        self.classifier = nn.ModuleList([nn.Linear(1280, meta_class_size) for _ in range(ensemble_size)])
+        self.classifier = nn.ModuleList([eca_mobilenet_v2(pretrained=True, last_channel=feature_dim,
+                                                          num_classes=meta_class_size).classifier for _ in
+                                         range(ensemble_size)])
+        print("# trainable individual classifier parameters:",
+              sum(param.numel() if param.requires_grad else 0 for param in
+                  self.classifier.parameters()) // ensemble_size)
 
     def forward(self, x):
-        batch_size = x.size(0)
         shared = self.head(x)
         features, out = [], []
         for i in range(self.ensemble_size):
             feature = self.tails[i](shared)
-            feature = F.adaptive_avg_pool2d(feature, output_size=(1, 1)).view(batch_size, -1)
+            feature = torch.flatten(F.adaptive_avg_pool2d(feature, output_size=(1, 1)), start_dim=1)
             features.append(feature)
             classes = self.classifier[i](feature)
             out.append(classes)
