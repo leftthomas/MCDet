1869affb97d40a90c68319f853f147bd1202b8e2
leftthomas <leftthomas@qq.com>
2020-02-25 17:35:42 +0800

    change norm key

--- model.py
+++ model.py
@@ -12,11 +12,11 @@ class WeightNorm2d(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, backbone_type, num_classes, bn_type):
+    def __init__(self, backbone_type, num_classes, norm_type):
         super(Model, self).__init__()
         backbones = {'resnet18': resnet18, 'resnet34': resnet34, 'resnet50': resnet50, 'resnext50': resnext50_32x4d}
-        bns = {'bn': nn.BatchNorm2d, 'in': nn.InstanceNorm2d, 'wn': WeightNorm2d}
-        self.backbone = backbones[backbone_type](num_classes=num_classes, norm_layer=bns[bn_type])
+        norms = {'bn': nn.BatchNorm2d, 'in': nn.InstanceNorm2d, 'wn': WeightNorm2d}
+        self.backbone = backbones[backbone_type](num_classes=num_classes, norm_layer=norms[norm_type])
 
     def forward(self, x):
         return self.backbone(x)
