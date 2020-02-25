2d5a348f2fa62c631ec9ebf9c38c2edf993fba08
leftthomas <leftthomas@qq.com>
2020-02-25 16:38:21 +0800

    finish train loop

--- cls.py
+++ cls.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import warnings
 
 import pandas as pd
@@ -8,11 +9,10 @@ import torch.optim as optim
 from thop import profile, clever_format
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
-from torchvision import datasets
 from tqdm import tqdm
 
-import utils
 from model import Model
+from utils import *
 
 warnings.filterwarnings("ignore")
 
@@ -57,24 +57,28 @@ if __name__ == '__main__':
                         choices=['resnet18', 'resnet34', 'resnet50', 'resnext50'], help='Backbone type')
     parser.add_argument('--with_bn', action='store_true', help='Add BN or not')
     parser.add_argument('--batch_size', type=int, default=256, help='Number of images in each mini-batch')
-    parser.add_argument('--epochs', type=int, default=100, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--epochs', type=int, default=90, help='Number of sweeps over the dataset to train')
 
     args = parser.parse_args()
     data_name, data_path, batch_size, epochs = args.data_name, args.data_path, args.batch_size, args.epochs
     backbone_type, with_bn = args.backbone_type, args.with_bn
-    train_data = datasets.ImageFolder(root='{}/{}'.format(data_path, 'train'), transform=utils.train_transform)
+    train_data = get_dataset(data_name, 'train', data_path)
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=16, pin_memory=True)
-    val_data = datasets.ImageFolder(root='{}/{}'.format(data_path, 'val'), transform=utils.val_transform)
+    val_data = get_dataset(data_name, 'val', data_path)
     val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=16, pin_memory=True)
 
-    model = Model(num_classes=1000).cuda()
-    flops, params = profile(model, inputs=(torch.randn(1, 3, 224, 224).cuda(),))
+    model = Model(backbone_type, num_classes=len(train_data.class_to_idx), with_bn=with_bn).cuda()
+    flops, params = profile(model, inputs=(torch.randn(1, 3, crop_size[data_name][-1],
+                                                       crop_size[data_name][-1]).cuda(),))
     flops, params = clever_format([flops, params])
     print('# Model Params: {} FLOPs: {}'.format(params, flops))
     optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
     lr_scheduler = StepLR(optimizer, step_size=30, gamma=0.1)
     loss_criterion = nn.CrossEntropyLoss()
     results = {'train_loss': [], 'train_acc@1': [], 'train_acc@5': [], 'val_loss': [], 'val_acc@1': [], 'val_acc@5': []}
+    save_name_pre = '{}_{}_{}'.format(data_name, backbone_type, 'bn' if with_bn else 'nbn')
+    if not os.path.exists('results'):
+        os.mkdir('results')
 
     best_acc = 0.0
     for epoch in range(1, epochs + 1):
@@ -89,7 +93,7 @@ if __name__ == '__main__':
         results['val_acc@5'].append(val_acc_5)
         # save statistics
         data_frame = pd.DataFrame(data=results, index=range(1, epoch + 1))
-        data_frame.to_csv('results/statistics.csv', index_label='epoch')
+        data_frame.to_csv('results/{}_statistics.csv'.format(save_name_pre), index_label='epoch')
         if val_acc_1 > best_acc:
             best_acc = val_acc_1
-            torch.save(model.state_dict(), 'results/model.pth')
+            torch.save(model.state_dict(), 'results/{}_model.pth'.format(save_name_pre))
--- utils.py
+++ utils.py
@@ -3,22 +3,20 @@ from torchvision.datasets import CIFAR10, CIFAR100, ImageFolder
 
 rgb_mean = {'cifar10': [0.491, 0.482, 0.447], 'cifar100': [0.507, 0.487, 0.441], 'imagenet': [0.485, 0.456, 0.406]}
 rgb_std = {'cifar10': [0.202, 0.199, 0.201], 'cifar100': [0.267, 0.256, 0.276], 'imagenet': [0.229, 0.224, 0.225]}
+crop_size = {'cifar10': [32, 32], 'cifar100': [32, 32], 'imagenet': [256, 224]}
+color_jitter_factor = {'cifar10': 0.5, 'cifar100': 0.5, 'imagenet': 1.0}
 dataset = {'cifar10': CIFAR10, 'cifar100': CIFAR100, 'imagenet': ImageFolder}
 
 
 def get_dataset(data_name, data_type='train', data_path=None):
-    if data_name == 'imagenet':
-        crop_size, color_jitter_factor = [256, 224], 1.0
-    else:
-        crop_size, color_jitter_factor = [32, 32], 0.5
     if data_type == 'train':
         transform = transforms.Compose([
             transforms.RandomResizedCrop(crop_size[data_name][-1]),
             transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomApply([transforms.ColorJitter(0.8 * color_jitter_factor,
-                                                           0.8 * color_jitter_factor,
-                                                           0.8 * color_jitter_factor,
-                                                           0.2 * color_jitter_factor)], p=0.8),
+            transforms.RandomApply([transforms.ColorJitter(0.8 * color_jitter_factor[data_name],
+                                                           0.8 * color_jitter_factor[data_name],
+                                                           0.8 * color_jitter_factor[data_name],
+                                                           0.2 * color_jitter_factor[data_name])], p=0.8),
             transforms.RandomGrayscale(p=0.2),
             transforms.ToTensor(),
             transforms.Normalize(rgb_mean[data_name], rgb_std[data_name])])
@@ -30,8 +28,6 @@ def get_dataset(data_name, data_type='train', data_path=None):
             transforms.Normalize(rgb_mean[data_name], rgb_std[data_name])])
 
     if data_name == 'imagenet':
-        return dataset[data_name](root='{}/{}'.format(data_path, 'train' if data_type == 'train' else 'val'),
-                                  transform=transform)
+        return dataset[data_name](root='{}/{}'.format(data_path, data_type), transform=transform)
     else:
-        return dataset[data_name](root='data', train=True if data_type == 'train' else False,
-                                  transform=transform, download=True)
+        return dataset[data_name](root='data', train=data_type == 'train', transform=transform, download=True)
