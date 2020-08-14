5b5621406f7f83899a5cbb4a579bb4db7e446e35
Hao Ren <leftthomas@qq.com>
2020-08-15 01:02:19 +0800

    fix bug about cache

--- mmdet/datasets/bdd100k.py
+++ mmdet/datasets/bdd100k.py
@@ -62,8 +62,10 @@ class BDD100KDataset(CocoDataset):
 
     def pre_pipeline(self, results):
         """Load real and gen images"""
-        results['img_info']['filename'] = ['real/' + results['img_info']['filename'],
-                                           'gen/' + results['img_info']['filename']]
+         # fix bug about cache
+        if not isinstance(img_info['filename'], list):
+            results['img_info']['filename'] = ['real/' + results['img_info']['filename'],
+                                               'gen/' + results['img_info']['filename']]
         results['img_prefix'] = self.img_prefix
         results['seg_prefix'] = self.seg_prefix
         results['proposal_file'] = self.proposal_file
